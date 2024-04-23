650c2c883fc5dda7812b99a1f8f698bfb7230411
Nghia Nguyen <Nghia.nd.librasoft@gmail.com>
2024-04-23 17:37:59 +0700

    add create document

---
+++ ScanApp.Service/Constracts/IDocumentService.cs
@@ -0,0 +1,16 @@
+﻿using ScanApp.Data.Entities;
+using ScanApp.Model.Requests.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanApp.Service.Constracts
+{
+    public interface IDocumentService
+    {
+        Task<int> Create(DocumentCreateRequest request);
+        Task<IEnumerable<Document>> GetAll();
+    }
+}
---
+++ ScanApp.Service/Services/DocumentService.cs
@@ -0,0 +1,55 @@
+﻿using ScanApp.Data.Entities;
+using ScanApp.Data.Infrastructure;
+using ScanApp.Data.Infrastructure.Interface;
+using ScanApp.Data.Repositories;
+using ScanApp.Model.Requests.Document;
+using ScanApp.Service.Constracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanApp.Service.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        private readonly IDocumentRepo _documentRepo;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DocumentService(ScanContext context) 
+        {
+            _documentRepo = new DocumentRepo(context);
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        public async Task<int> Create(DocumentCreateRequest request)
+        {
+            try
+            {
+                Document document = new Document()
+                {
+                    BatchId = request.BatchId,
+                    DocumentName = request.DocumentName,
+                    DocumentPath = request.DocumentPath,
+                    Note = request.Note,
+                    CreatedDate = request.CreatedDate
+                };
+
+                await _documentRepo.AddAsync(document);
+                await _unitOfWork.Save();
+
+                return document.Id;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Document>> GetAll()
+        {
+            return await _documentRepo.GetAllAsync();
+        }
+    }
+}
