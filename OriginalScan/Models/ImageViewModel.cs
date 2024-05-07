3d7378bc9eaf55b055dc4c64ab5b31e181e0ccf4
Nghia Nguyen <Nghia.nd.librasoft@gmail.com>
2024-05-07 16:26:07 +0700

    Update source: add ImageViewModel

---
+++ OriginalScan/Models/ImageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OriginalScan.Models
+{
+    public class ImageViewModel
+    {
+        public int Id { get; set; }
+
+        public int DocumentId { get; set; }
+
+        public string ImagePath { get; set; } = null!;
+
+        public bool IsSelected { get; set; }
+    }
+}
